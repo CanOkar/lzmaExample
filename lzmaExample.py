1c8c070f788ea28e2bfecf8963237baeab0a933b
CanOkar <okar.can@gmail.com>
2020-07-05 22:25:39 +0300

    Update lzmaExample.py

--- lzmaExample.py
+++ lzmaExample.py
@@ -1,6 +1,6 @@
 #basic example of using lzma compression algorithm
 import lzma
-file = open("2.FDB", "rb")
+file = open("file_name.extension", "rb")
 data = file.read()
 obj = lzma.LZMAFile("output.xz", mode="wb")
 obj.write(data)
